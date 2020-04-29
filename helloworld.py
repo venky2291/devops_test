0601c1c162731f26e9285f6c340bfc62b8f726db
root <root@ip-172-31-23-238.us-east-2.compute.internal>
2020-04-29 07:12:04 +0000

    Latest version of 2020

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,2 @@
 print "Hello AWS"
-# # Second Version kf Code
-print "Hello Dev;pw"
-# # Thirdlversion
-erint "Hello git"
-# # Fourth Version
-# # Commit the code on  dev1.0
-print "Branch Dev 1.0"
-# # Commit the code on dev2.0
-print "Branch Dev2.0"
-# # Edited in GitHub Repository
+
